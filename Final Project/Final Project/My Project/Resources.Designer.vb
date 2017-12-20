ab3dc26df1d7eb266586361ea066465969583b89
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-12-20 09:47:22 -0600

    Finished

--- Final Project/Final Project/My Project/Resources.Designer.vb
+++ Final Project/Final Project/My Project/Resources.Designer.vb
@@ -163,6 +163,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property pets_family_friend_newborn_baby_sonny_fb() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pets-family-friend-newborn-baby-sonny-fb", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property tmp592173530683015168() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("tmp592173530683015168", resourceCulture)
--- Final Project/Final Project/Pets Owned.Designer.vb
+++ Final Project/Final Project/Pets Owned.Designer.vb
@@ -25,9 +25,11 @@ Partial Class Matching
         Me.components = New System.ComponentModel.Container()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.btnAverage = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblPetsAverage = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Timer1
@@ -43,14 +45,6 @@ Partial Class Matching
         Me.Label1.Size = New System.Drawing.Size(148, 40)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Average amount of " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "       pets per family:"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(176, 61)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(144, 29)
-        Me.TextBox1.TabIndex = 1
         '
         'btnAverage
         '
@@ -72,25 +66,46 @@ Partial Class Matching
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
+        'lblPetsAverage
+        '
+        Me.lblPetsAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPetsAverage.Location = New System.Drawing.Point(166, 61)
+        Me.lblPetsAverage.Name = "lblPetsAverage"
+        Me.lblPetsAverage.Size = New System.Drawing.Size(168, 36)
+        Me.lblPetsAverage.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Final_Project.My.Resources.Resources.pets_family_friend_newborn_baby_sonny_fb
+        Me.PictureBox1.Location = New System.Drawing.Point(340, 45)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(224, 143)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
         'Matching
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(376, 214)
+        Me.ClientSize = New System.Drawing.Size(559, 214)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblPetsAverage)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnAverage)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Matching"
         Me.Text = "Final Project"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Timer1 As System.Windows.Forms.Timer
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents btnAverage As System.Windows.Forms.Button
     Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblPetsAverage As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
 
 End Class
